c8105c8dc379967d5816392929a261035b308e35
Dave Jones <dave@waveform.org.uk>
2015-10-05 22:17:42 +0100

    Fix #44
    Implement native Debian packaging (control files are part of the source
    repo) with Makefile to simplify the release procedure.

--- setup.py
+++ setup.py
@@ -1,37 +1,88 @@
 import os
+import sys
 from setuptools import setup, find_packages
 
+"A simple interface to everyday GPIO components used with Raspberry Pi"
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+if sys.version_info[0] == 2:
+    if not sys.version_info >= (2, 7):
+        raise ValueError('This package requires Python 2.7 or above')
+elif sys.version_info[0] == 3:
+    if not sys.version_info >= (3, 2):
+        raise ValueError('This package requires Python 3.2 or above')
+else:
+    raise ValueError('Unrecognized major version of Python')
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Workaround <http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html>
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+__project__      = 'gpiozero'
+__version__      = '0.7.0'
+__author__       = 'Ben Nuttall'
+__author_email__ = 'ben@raspberrypi.org'
+__url__          = 'https://github.com/RPi-Distro/python-gpiozero'
+__platforms__    = 'ALL'
+
+__classifiers__ = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Education",
+    "Intended Audience :: Developers",
+    "Topic :: Education",
+    "Topic :: System :: Hardware",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+]
+
+__keywords__ = [
+    'raspberrypi',
+    'gpio',
+]
+
+__requires__ = [
+    'RPi.GPIO',
+    'w1thermsensor',
+    'spidev',
+]
+
+__extra_requires__ = {
+}
+
+__entry_points__ = {
+}
 
 
-setup(
-    name="gpiozero",
-    version="0.7.0",
-    author="Ben Nuttall",
-    description="A simple interface to everyday GPIO components used with Raspberry Pi",
-    license="BSD",
-    keywords=[
-        "raspberrypi",
-        "gpio",
-    ],
-    url="https://github.com/RPi-Distro/gpio-zero",
-    packages=find_packages(),
-    install_requires=[
-        "RPi.GPIO",
-        "w1thermsensor",
-        "spidev",
-    ],
-    long_description=read('README.rst'),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Education",
-        "Intended Audience :: Developers",
-        "Topic :: Education",
-        "Topic :: System :: Hardware",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-    ],
-)
+def main():
+    import io
+    with io.open(os.path.join(HERE, 'README.rst'), 'r') as readme:
+        setup(
+            name                 = __project__,
+            version              = __version__,
+            description          = __doc__,
+            long_description     = readme.read(),
+            classifiers          = __classifiers__,
+            author               = __author__,
+            author_email         = __author_email__,
+            url                  = __url__,
+            license              = [
+                c.rsplit('::', 1)[1].strip()
+                for c in __classifiers__
+                if c.startswith('License ::')
+            ][0],
+            keywords             = __keywords__,
+            packages             = find_packages(),
+            include_package_data = True,
+            platforms            = __platforms__,
+            install_requires     = __requires__,
+            extras_require       = __extra_requires__,
+            entry_points         = __entry_points__,
+        )
+
+
+if __name__ == '__main__':
+    main()
