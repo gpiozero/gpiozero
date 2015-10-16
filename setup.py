fcc7c47fe3c6b4017be445fbf29cac5718eba05b
Ben Nuttall <ben@bennuttall.com>
2015-10-16 11:40:42 +0100

    Release v0.8.0 beta 3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except ImportError:
     pass
 
 __project__      = 'gpiozero'
-__version__      = '0.7.0'
+__version__      = '0.8.0'
 __author__       = 'Ben Nuttall'
 __author_email__ = 'ben@raspberrypi.org'
 __url__          = 'https://github.com/RPi-Distro/python-gpiozero'
