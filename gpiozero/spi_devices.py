cd879589950cef41f91c58d2c0032b803022acea
Ben Nuttall <ben@bennuttall.com>
2016-05-26 17:32:37 +0100

    Add ADC.voltage property, fix #329

--- gpiozero/spi_devices.py
+++ gpiozero/spi_devices.py
@@ -113,11 +113,14 @@ class AnalogInputDevice(SPIDevice):
     .. _analog to digital converters: https://en.wikipedia.org/wiki/Analog-to-digital_converter
     """
 
-    def __init__(self, bits, **spi_args):
+    def __init__(self, bits, max_voltage=3.3, **spi_args):
+        if bits is None:
+            raise InputDeviceError('you must specify the bit resolution of the device')
         self._bits = bits
         self._min_value = -(2 ** bits)
         self._range = 2 ** (bits + 1) - 1
         super(AnalogInputDevice, self).__init__(shared=True, **spi_args)
+        self._max_voltage = max_voltage
 
     @property
     def bits(self):
@@ -143,6 +146,13 @@ class AnalogInputDevice(SPIDevice):
         The raw value as read from the device.
         """
         return self._read()
+
+    @property
+    def voltage(self):
+        """
+        The current voltage read from the device
+        """
+        return self.value * self._max_voltage
 
 
 class MCP3xxx(AnalogInputDevice):
