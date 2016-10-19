a5f7b7eb4c646148a9e9d41931a352dbf5e0e3a0
Martin OHanlon <martin@ohanlonweb.com>
2016-10-19 22:37:08 +0000

    7 segment updates

--- gpiozero/boards.py
+++ gpiozero/boards.py
@@ -1186,7 +1186,45 @@ class Energenie(SourceMixin, Device):
         self.value = False
 
 class SevenSegmentDisplay(LEDBoard):
+    """
+    Extends :class:`LEDBoard` for a 7 segment LED display 
+
+    7 segment displays have either 7 or 8 pins, 7 pins for the digit display 
+    and an optional 8th pin for a decimal point. 7 segment displays 
+    typically have either a common anode or common cathode pin, when
+    using a common anode display 'active_high' should be set to False.
+    Instances of this class can be used to display characters or control 
+    individual leds on the display. For example::
+
+        from gpiozero import SevenSegmentDisplay
+
+        seven = SevenSegmentDisplay(1,2,3,4,5,6,7,8,active_high=False)
+        seven.display("7")
     
+    :param int \*pins:
+        Specify the GPIO pins that the 7 segment display is attached to.
+        Pins should be in the LED segment order A,B,C,D,E,F,G,decimal_point 
+        (the decimal_point is optional).
+
+    :param bool pwm:
+        If ``True``, construct :class:`PWMLED` instances for each pin. If
+        ``False`` (the default), construct regular :class:`LED` instances. This
+        parameter can only be specified as a keyword parameter.
+
+    :param bool active_high:
+        If ``True`` (the default), the :meth:`on` method will set all the
+        associated pins to HIGH. If ``False``, the :meth:`on` method will set
+        all pins to LOW (the :meth:`off` method always does the opposite). This
+        parameter can only be specified as a keyword parameter.
+
+    :param bool initial_value:
+        If ``False`` (the default), all LEDs will be off initially. If
+        ``None``, each device will be left in whatever state the pin is found
+        in when configured for output (warning: this can be on). If ``True``,
+        the device will be switched on initially. This parameter can only be
+        specified as a keyword parameter.    
+    
+    """
     def __init__(self, *pins, **kwargs):
         # 7 segment displays must have 7 or 8 pins
         if len(pins) < 7 or len(pins) > 8:
@@ -1245,6 +1283,12 @@ class SevenSegmentDisplay(LEDBoard):
         super(SevenSegmentDisplay, self).__init__(*pins, pwm=pwm, active_high=active_high, initial_value=initial_value)
     
     def display(self, char):
+        """
+        Display a character on the 7 segment display
+
+        :param string char:
+            A single character to be displayed 
+        """
         char = str(char).upper()
         if len(char) > 1:
             raise ValueError('only a single character can be displayed')
@@ -1255,10 +1299,19 @@ class SevenSegmentDisplay(LEDBoard):
             self[led].value = layout[led]
             
     def display_hex(self, hexnumber):
+        """
+        Display a hex number (0-F) on the 7 segment display
+
+        :param int hexnumber:
+            The number to be displayed in hex 
+        """
         self.display(hex(hexnumber)[2:])
 
     @property
     def decimal_point(self):
+        """
+        Represents the status of the decimal point led
+        """
         # does the 7seg display have a decimal point (i.e pin 8)
         if len(self) > 7:
             return self[7].value 
@@ -1267,13 +1320,26 @@ class SevenSegmentDisplay(LEDBoard):
     
     @decimal_point.setter
     def decimal_point(self, value):
+        """
+        Sets the status of the decimal point led
+        """
         if len(self) > 7:
             self[7].value = value
         else:
-            raise OutputDeviceError('there is no 8th pin for the decimal point')
-            
+            raise OutputDeviceError('there is no 8th pin for the decimal point')    
+    
     def set_char_layout(self, char, layout):
-        char = str(char)
+        """
+        Create or update a custom character layout, which can be used with the 
+        `display` method.
+
+        :param string char:
+            A single character to be displayed 
+            
+        :param tuple layout:
+            A 7 bool tuple of LED values in the segment order A, B, C, D, E, F, G
+        """
+        char = str(char).upper()
         if len(char) != 1:
             raise ValueError('only a single character can be used in a layout')
         if len(layout) != 7:
