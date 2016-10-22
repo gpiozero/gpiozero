37ff6e5000c133307d67adeb9fdb72c8a6c506e3
Martin OHanlon <martin@ohanlonweb.com>
2016-10-22 02:30:43 +0000

    seven segment test

--- tests/test_boards.py
+++ tests/test_boards.py
@@ -769,13 +769,17 @@ def test_seven_segment_display():
         assert seven_seg[5].active_high
         assert seven_seg[6].active_high
         assert seven_seg[7].active_high
+
         seven_seg.display("8")
         assert (seven_seg[0].value and seven_seg[1].value and seven_seg[2].value and seven_seg[3].value and seven_seg[4].value and seven_seg[5].value and seven_seg[6].value)
+
         seven_seg.display_hex(15)
         assert (seven_seg[0].value and not seven_seg[1].value and not seven_seg[2].value and not seven_seg[3].value and seven_seg[4].value and seven_seg[5].value and seven_seg[6].value)
+
         seven_seg.set_char_layout("_", (False, False, False, True, False, False, False))
         seven_seg.display("_")
         assert (not seven_seg[0].value and not seven_seg[1].value and not seven_seg[2].value and seven_seg[3].value and not seven_seg[4].value and not seven_seg[5].value and not seven_seg[6].value)
+
         seven_seg.decimal_point = True
         assert seven_seg[7].value
         assert seven_seg.decimal_point
@@ -807,13 +811,17 @@ def test_seven_segment_display_active_low():
         assert not seven_seg[5].active_high
         assert not seven_seg[6].active_high
         assert not seven_seg[7].active_high
+
         seven_seg.display("8")
         assert (seven_seg[0].value and seven_seg[1].value and seven_seg[2].value and seven_seg[3].value and seven_seg[4].value and seven_seg[5].value and seven_seg[6].value)
+
         seven_seg.display_hex(15)
+        assert (seven_seg[0].value and not seven_seg[1].value and not seven_seg[2].value and not seven_seg[3].value and seven_seg[4].value and seven_seg[5].value and seven_seg[6].value)
+
         seven_seg.set_char_layout("_", (False, False, False, True, False, False, False))
         seven_seg.display("_")
         assert (not seven_seg[0].value and not seven_seg[1].value and not seven_seg[2].value and seven_seg[3].value and not seven_seg[4].value and not seven_seg[5].value and not seven_seg[6].value)
-        assert (seven_seg[0].value and not seven_seg[1].value and not seven_seg[2].value and not seven_seg[3].value and seven_seg[4].value and seven_seg[5].value and seven_seg[6].value)
+
         seven_seg.decimal_point = True
         assert seven_seg[7].value
         assert seven_seg.decimal_point
@@ -834,4 +842,20 @@ def test_seven_segment_display_bad_init():
         SevenSegmentDisplay(pin1, pin2, pin3, pin4, pin5, pin6)
     with pytest.raises(ValueError):
         SevenSegmentDisplay(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9)
+        
+def test_seven_segment_display_bad_display():
+    pin1 = MockPin(2)
+    pin2 = MockPin(3)
+    pin3 = MockPin(4)
+    pin4 = MockPin(5)
+    pin5 = MockPin(6)
+    pin6 = MockPin(7)
+    pin7 = MockPin(8)
+    pin8 = MockPin(9)
+    pin9 = MockPin(10)
+    with SevenSegmentDisplay(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8) as seven_seg:
+        with pytest.raises(ValueError):
+            seven_seg.display("AA")
+        with pytest.raises(ValueError):
+            seven_seg.display("%")
         
