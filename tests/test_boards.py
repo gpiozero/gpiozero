78ea8e9a7d03bd6f8df1a40cc4daddad875b42af
Martin OHanlon <martin@ohanlonweb.com>
2016-10-23 13:02:45 +0000

    refactor tests to not use pin 2 and 3

--- tests/test_boards.py
+++ tests/test_boards.py
@@ -743,14 +743,14 @@ def test_energenie():
         assert repr(device1) == '<gpiozero.Energenie object closed>'
 
 def test_seven_segment_display():
-    pin1 = MockPin(2)
-    pin2 = MockPin(3)
-    pin3 = MockPin(4)
-    pin4 = MockPin(5)
-    pin5 = MockPin(6)
-    pin6 = MockPin(7)
-    pin7 = MockPin(8)
-    pin8 = MockPin(9)
+    pin1 = MockPin(4)
+    pin2 = MockPin(5)
+    pin3 = MockPin(6)
+    pin4 = MockPin(7)
+    pin5 = MockPin(8)
+    pin6 = MockPin(9)
+    pin7 = MockPin(10)
+    pin8 = MockPin(11)
     with SevenSegmentDisplay(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8) as seven_seg:
         assert isinstance(seven_seg[0], LED)
         assert isinstance(seven_seg[1], LED)
@@ -785,14 +785,14 @@ def test_seven_segment_display():
         assert seven_seg.decimal_point
         
 def test_seven_segment_display_active_low():
-    pin1 = MockPin(2)
-    pin2 = MockPin(3)
-    pin3 = MockPin(4)
-    pin4 = MockPin(5)
-    pin5 = MockPin(6)
-    pin6 = MockPin(7)
-    pin7 = MockPin(8)
-    pin8 = MockPin(9)
+    pin1 = MockPin(4)
+    pin2 = MockPin(5)
+    pin3 = MockPin(6)
+    pin4 = MockPin(7)
+    pin5 = MockPin(8)
+    pin6 = MockPin(9)
+    pin7 = MockPin(10)
+    pin8 = MockPin(11)
     with SevenSegmentDisplay(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, active_high=False) as seven_seg:
         assert isinstance(seven_seg[0], LED)
         assert isinstance(seven_seg[1], LED)
@@ -827,14 +827,14 @@ def test_seven_segment_display_active_low():
         assert seven_seg.decimal_point
 
 def test_seven_segment_initial_value():
-    pin1 = MockPin(2)
-    pin2 = MockPin(3)
-    pin3 = MockPin(4)
-    pin4 = MockPin(5)
-    pin5 = MockPin(6)
-    pin6 = MockPin(7)
-    pin7 = MockPin(8)
-    pin8 = MockPin(9)
+    pin1 = MockPin(4)
+    pin2 = MockPin(5)
+    pin3 = MockPin(6)
+    pin4 = MockPin(7)
+    pin5 = MockPin(8)
+    pin6 = MockPin(9)
+    pin7 = MockPin(10)
+    pin8 = MockPin(11)
     with SevenSegmentDisplay(pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, initial_value=True) as seven_seg:
         assert (seven_seg[0].value and seven_seg[1].value and seven_seg[2].value and seven_seg[3].value and seven_seg[4].value and seven_seg[5].value and seven_seg[6].value and seven_seg[7].value)
         
