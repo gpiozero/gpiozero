11aa01136c4420264fc4b48a732280c972d1b1df
Ben Nuttall <ben@bennuttall.com>
2018-09-22 11:25:57 +0100

    Fix error in RGBLED example

--- gpiozero/boards.py
+++ gpiozero/boards.py
@@ -1065,7 +1065,7 @@ class RGBLEDBoard(CompositeOutputDevice):
 
         from gpiozero import RGBLEDBoard
 
-        leds = RGBLEDBoard((2, 3, 4), (5, 6, 7), (7, 8, 9))
+        leds = RGBLEDBoard((2, 3, 4), (5, 6, 7), (8, 9, 10))
 
         leds.value = ((1, 0, 0), (0, 1, 0), (0, 0, 1))
 
