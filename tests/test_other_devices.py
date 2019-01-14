1ca8abc904ece7e2a17a519fe15a544081a2b1de
Ben Nuttall <ben@bennuttall.com>
2019-01-14 15:45:45 +0000

    Update test_other_devices.py

--- tests/test_other_devices.py
+++ tests/test_other_devices.py
@@ -13,24 +13,24 @@ import pytest
 from gpiozero import *
 from datetime import time
 
-def test_PingServer_init():
+def test_pingserver_init():
     with PingServer('localhost') as server:
         assert server.host == 'localhost'
 
-def test_PingServer_repr():
+def test_pingserver_repr():
     with PingServer('localhost') as server:
         assert repr(server) == '<gpiozero.PingServer host="%s">' % server.host
 
-def test_PingServer_unknown_attr():
+def test_pingserver_unknown_attr():
     with PingServer('localhost') as server:
         with pytest.raises(AttributeError):
             server.foo = 1
 
-def test_TimeOfDay_init():
+def test_timeofday_init():
     with TimeOfDay(time(7), time(12), utc=False) as morning:
         assert morning.start_time == time(7)
         assert morning.end_time == time(12)
         assert morning.utc == False
         morning.utc = True
         assert morning.utc == True
-
+        
