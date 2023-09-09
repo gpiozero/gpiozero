fd3f4c2ba8c3fbfc17ab3efa46dfb76487337cff
Dave Jones <dave@waveform.org.uk>
2023-09-10 00:08:23 +0100

    Clean up doc config
    Don't rely on setuptools (more and more of which is deprecated by the
    day), excise all the stuff that's default anyway, use f-strings,
    generate man-page links to the current debian suite, bump the
    intersphinx Python links to 3.9 (our base version for this release), and
    add a man-page entry for pintest

--- docs/conf.py
+++ docs/conf.py
@@ -11,45 +11,40 @@
 
 import sys
 import os
+import configparser
 from pathlib import Path
 from datetime import datetime
 from setuptools.config import read_configuration
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-config = read_configuration(str(Path(__file__).parent / '..' / 'setup.cfg'))
+on_rtd = os.environ.get('READTHEDOCS', '').lower() == 'true'
+config = configparser.ConfigParser()
+config.read([Path(__file__).parent / '..' / 'setup.cfg'])
 info = config['metadata']
+
+# -- Project information -----------------------------------------------------
+
+project = info['name']
+author = info['author']
+copyright = f'2015-{datetime.now():%Y} {author}'
+release = info['version']
+version = release
 
 # -- General configuration ------------------------------------------------
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
+needs_sphinx = '1.4.0'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.imgmath',
+]
 if on_rtd:
-    needs_sphinx = '1.4.0'
-    extensions.append('sphinx.ext.imgmath')
-    imgmath_image_format = 'svg'
     tags.add('rtd')
-else:
-    extensions.append('sphinx.ext.mathjax')
-    mathjax_path = '/usr/share/javascript/mathjax/MathJax.js?config=TeX-AMS_HTML'
 
 templates_path = ['_templates']
-source_suffix = '.rst'
-#source_encoding = 'utf-8-sig'
-master_doc = 'index'
-project = info['name']
-copyright = '2015-{now:%Y} {info[author]}'.format(now=datetime.now(), info=info)
-version = info['version']
-#release = None
-#language = None
-#today_fmt = '%B %d, %Y'
 exclude_patterns = ['_build']
-highlight_language='python3'
-#default_role = None
-#add_function_parentheses = True
-#add_module_names = True
-#show_authors = False
+highlight_language = 'python3'
 pygments_style = 'sphinx'
-#modindex_common_prefix = []
-#keep_warnings = False
 
 # -- Autodoc configuration ------------------------------------------------
 
@@ -67,38 +62,17 @@ autodoc_mock_imports = [
 # -- Intersphinx configuration --------------------------------------------
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.7', None),
+    'python': ('https://docs.python.org/3.9', None),
     'picamera': ('https://picamera.readthedocs.io/en/latest', None),
     'colorzero': ('https://colorzero.readthedocs.io/en/latest', None),
-    }
+}
 
 # -- Options for HTML output ----------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_title = '{info[name]} {info[version]} Documentation'.format(info=info)
-#html_theme_path = []
-#html_short_title = None
-#html_logo = None
-#html_favicon = None
+html_title = f'{project} {version} Documentation'
 html_static_path = ['_static']
-#html_extra_path = []
-#html_last_updated_fmt = '%b %d, %Y'
-#html_use_smartypants = True
-#html_additional_pages = {}
-#html_domain_indices = True
-#html_use_index = True
-#html_split_index = False
-#html_show_sourcelink = True
-#html_show_sphinx = True
-#html_show_copyright = True
-#html_use_opensearch = ''
-#html_file_suffix = None
-htmlhelp_basename = '{info[name]}doc'.format(info=info)
-
-# Hack to make wide tables work properly in RTD
-# See https://github.com/snide/sphinx_rtd_theme/issues/117 for details
-#def setup(app):
-#    app.add_stylesheet('style_override.css')
+manpages_url = 'https://manpages.debian.org/bookworm/{page}.{section}.en.html'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -115,47 +89,31 @@ latex_documents = [
         'index',            # source start file
         project + '.tex',   # target filename
         html_title,         # title
-        info['author'],     # author
+        author,             # author
         'manual',           # documentclass
         True,               # documents ref'd from toctree only
-        ),
+    ),
 ]
 
-#latex_logo = None
-#latex_use_parts = False
 latex_show_pagerefs = True
 latex_show_urls = 'footnote'
-#latex_appendices = []
-#latex_domain_indices = True
 
 # -- Options for epub output ----------------------------------------------
 
 epub_basename = project
-#epub_theme = 'epub'
-#epub_title = html_title
-epub_author = info['author']
-epub_identifier = 'https://{info[name]}.readthedocs.io/'.format(info=info)
-#epub_tocdepth = 3
+epub_author = author
+epub_identifier = f'https://{info["name"]}.readthedocs.io/'
 epub_show_urls = 'no'
-#epub_use_index = True
 
 # -- Options for manual page output ---------------------------------------
 
 man_pages = [
     ('cli_pinout',  'pinout',      'GPIO Zero pinout tool',       [info['author']], 1),
+    ('cli_pintest', 'pintest',     'GPIO Zero pintest tool',      [info['author']], 1),
     ('remote_gpio', 'remote-gpio', 'GPIO Zero remote GPIO guide', [info['author']], 7),
 ]
 
 man_show_urls = True
-
-# -- Options for Texinfo output -------------------------------------------
-
-texinfo_documents = []
-
-#texinfo_appendices = []
-#texinfo_domain_indices = True
-#texinfo_show_urls = 'footnote'
-#texinfo_no_detailmenu = False
 
 # -- Options for linkcheck builder ----------------------------------------
 
