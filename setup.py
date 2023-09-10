595bbd8f594989dcb485e89e8c2f6c8da6c877af
Dave Jones <dave@waveform.org.uk>
2023-09-10 23:41:34 +0100

    More removal of deprecated stuff
    Also, we probably don't want to be installing RPi.GPIO or pigpio unless
    specifically requested after this version

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-"""A simple interface to GPIO devices with Raspberry Pi."""
+from setuptools import setup
 
-import io
-import os
-import errno
-from setuptools import setup, config
-
-
-cfg = config.read_configuration(
-    os.path.join(os.path.dirname(__file__), 'setup.cfg'))
-
-try:
-    # If we're executing on a Raspberry Pi, install all GPIO libraries for
-    # testing (except RPIO which doesn't work on the multi-core models yet)
-    with io.open('/proc/device-tree/model', 'r') as f:
-        if f.read().startswith('Raspberry Pi'):
-            cfg['options']['extras_require']['test'].append('RPI.GPIO')
-            cfg['options']['extras_require']['test'].append('pigpio')
-except IOError as e:
-    if e.errno != errno.ENOENT:
-        raise
-
-opts = cfg['metadata']
-opts.update(cfg['options'])
-
-setup(**opts)
+setup()
